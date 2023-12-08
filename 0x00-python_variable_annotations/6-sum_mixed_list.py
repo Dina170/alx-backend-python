98666f625c540a7e12692ee301946f86f5f6ef09
dina <dinaelhadidy966@gmail.com>
2023-12-08 08:51:11 -0500

    Replace | with , in Union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns the sum of floats and integers"""
     return sum(mxd_lst)
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,6 +8,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple of string and float"""
     return (k, float(v * v))
