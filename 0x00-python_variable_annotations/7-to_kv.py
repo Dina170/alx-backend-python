ff6b213041b515fd5cb3c8f9654dc13b62da8cd9
dina <dinaelhadidy966@gmail.com>
2023-12-08 08:48:07 -0500

    Add tuple parentheses

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -10,4 +10,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
     """Returns a tuple of string and float"""
-    return k, float(v * v)
+    return (k, float(v * v))
